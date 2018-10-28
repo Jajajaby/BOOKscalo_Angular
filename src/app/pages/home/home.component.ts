9e01934974f1ac4e4e0126eb8feeaa1d6bf696b9
Javiera Oremeño Leiva <javieraormeno.l@gmail.com>
2018-10-28 20:03:03 -0300

    detalles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,20 +12,19 @@ export class HomeComponent implements OnInit {
 
 	constructor( private _dbService:DatabaseService ) {
 		this._dbService.getData('books')
-				.valueChanges()
-				.subscribe( data => {
-					this.books = [];
-					this.books = data;
-					
-					// cambia la referencia de user por el objeto del usuario
-					for(let i=0; i<this.books.length; i++){
-						this._dbService.afs.doc(data[i].user)
+			.valueChanges()
+			.subscribe( data => {
+				this.books = [];
+				this.books = data;
+			
+				// cambia la referencia de user por el objeto del usuario
+				for(let i=0; i<this.books.length; i++){
+					this._dbService.afs.doc(data[i].user)
 						.valueChanges()
 						.subscribe( resp => this.books[i].user = resp );
-					}
-				});
+				}
+			});
 	}
-
 
 	ngOnInit() {
 		this.view_bar = 'all';
