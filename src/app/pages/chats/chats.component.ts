4d64d0f020a3aaa10dc667a75f7711767aa57100
Javiera Oremeño Leiva <javieraormeno.l@gmail.com>
2018-11-11 23:29:06 -0300

    editando para que los chats se puedan responder

--- src/app/pages/chats/chats.component.ts
+++ src/app/pages/chats/chats.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { map } from 'rxjs/operators';
+
+// ANGULARFIRE2
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+
+// SERVICES
+import { DateService } from '../../services/date.service';
+import { DatabaseService } from 'src/app/services/database.service';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-chats',
@@ -23,10 +29,14 @@ export class ChatsComponent implements OnInit {
   };
   
   uid:string;
+  actual_user:string;
   
-  constructor( private afs:AngularFirestore ) { 
+  constructor(  private _dbService:DatabaseService,
+                private afs:AngularFirestore,
+                private _date:DateService ) { 
 
     this.uid = JSON.parse( localStorage.getItem('user') ).uid;
+    this.actual_user = JSON.parse( localStorage.getItem('user') );
    
     this.chatsCollection = afs.collection<any>('messages-transaction');
     
@@ -44,11 +54,27 @@ export class ChatsComponent implements OnInit {
   }
 
   showMessages( key:string ){
-    // console.log(key);
     this.selected_chat = this.afs.collection<any>('messages-transaction').doc(key);
     this.selected_chat
       .valueChanges()
       .subscribe( data => this.message = data );
   }
+
+
+  // Envía el mensaje desde el usuario actual hacia el usuario dueño del libro
+	sendMessage(){
+     let answer_msg = new FormGroup({
+      text: 						new FormControl([], Validators.required),
+			day: 							new FormControl(this._date.actual_day),
+			hour: 						new FormControl(this._date.actual_hour),
+     });
+
+     // Envía el mensaje a la DB.
+    //  updateData( collection:string, id:string, document:any )
+	// 	this._dbService.updateData('messages-transaction', predet_Message)
+	// 		.then( () => swal('Mensaje enviado', 'Su mensaje ha sido enviado con éxito', 'success') )
+	// 		.catch( () => swal('Error', 'Su mensaje no ha podido enviarse, vuelva a intentarlo', 'error') );
+	// }
+    }
 
 }
