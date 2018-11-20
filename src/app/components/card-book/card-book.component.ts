8ddbf458741f522db1c1768dd610be91ce9e7100
Matias Espinoza <mespinoza@ubicomp-labs.com>
2018-11-19 21:54:11 -0300

    no se permite enviar un mensaje si no estan los campos llenos en el card-book

--- src/app/components/card-book/card-book.component.ts
+++ src/app/components/card-book/card-book.component.ts
@@ -94,7 +94,10 @@ export class CardBookComponent implements OnInit {
 			status:											false,
 			price:											this.book_modal.price
 		}
-		console.log(predet_Message.pref);
+
+		if(predet_Message.pref === null ){
+			return swal('Error al enviar un mensaje', 'Debe completar todos los campos', 'error');
+		}
 	
 
 		if ( this.form.value.transaction == undefined || this.form.value.transaction === null){
@@ -117,8 +120,6 @@ export class CardBookComponent implements OnInit {
 			}
 			];
 		}
-
-		console.log(predet_Message.pref);
 
 		// Envía el mensaje a la DB.
 		this._dbService.addData('messages-transaction', predet_Message)
