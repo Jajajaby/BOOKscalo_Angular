aed2b08078416ebd5d8fa735d9604c2c5a381116
Matias Espinoza <mespinoza@ubicomp-labs.com>
2018-12-13 11:50:46 -0300

    se arreglaron las categorias de add-book

--- src/app/pages/add-book/add-book.component.ts
+++ src/app/pages/add-book/add-book.component.ts
@@ -29,9 +29,8 @@ export class AddBookComponent implements OnInit {
 	uid:string; // uid usuario actual
 	today:any;
 
-	categories:Array<string>=["Antiguedades y Coleccionables", "Arquitectura", "Arte", "Artes Escénicas", "Biografía y Autobiografía", "Casa y Hogar", "Ciencia", "Ciencias Políticas", "Ciencias Sociales", "Cocina", "Comida y Bebestibles", "Colecciones Literarias", "Cómics y Novelas Gráficas", "Computación e Internet", "Crímenes", "Crítica Literaria", "Cuerpo", "Mente y Espíritu", "Deportes y Recreación", "Drama", "Educación", "Estudio de Lenguas Extranjeras", "Ensayos Académicos", "Familia y Relaciones", "Ficción", "Ficción Adolescente", "Ficción para Niños", "Filosofía", "Fotografía", "Historia y Geografía", "Humor", "Jardinería", "Juegos", "Lectura escolar", "Lengua y Literatura", "Leyes", "Manualidades y Hobbies", "Mascotas y Animales", "Matemáticas", "Medicina", "Música", "Naturaleza y Aire libre", "Negocios y Economía", "Niños y Jóvenes", "Papelería", "Poesía", "Psicología", "Religión y Espiritualidad", "Salud y Bienestar", "Tecnología", "Transporte", "Viajes"];
-
-	selected_categories:string[] = [];
+	categories = [...CATEGORIES];
+	selected_categories:any[] = [];
 
 	constructor( 	private _dbService:DatabaseService,
 								private _date:DateService,
@@ -162,15 +161,13 @@ export class AddBookComponent implements OnInit {
 	// Agrega una categoría al cuadro de categorías del libro
 	addCategory(index:number){
 		this.selected_categories.push(this.categories[index]);
-		this.categories.splice(index, 1);				
-		// TODO: Ordenar por index
+		this.categories.splice(index, 1);
 	}
 
 	// Remueve una categoría del cuadro de categorías del libro
 	removeCategory(index:number){
 		this.categories.push(this.selected_categories[index]);
-		this.selected_categories.splice(index, 1);
-		console.log(this.selected_categories);				
+		this.selected_categories.splice(index, 1);				
 	}
 }
 
